86c8ef5616398a6908dd57e33daab5151019f52a
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-11-15 08:44:42 +0900

    update: basic code

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import BottomTabNavigator from './src/BottomTabNavigator';
+
+const MyDarkTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#000', // 기본 배경을 검정색으로 설정
+  },
+};
+
+export default function App() {
+  return (
+    <NavigationContainer theme={MyDarkTheme}>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+}
